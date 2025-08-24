cd5468aef9e9f1fd37ffe2fa734f99c59f013719
Krushna Wagh <wagh.krushna2004@gmail.com>
2025-08-24 23:38:12 +0530

    Forntend

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').Config} */
+export default {
+  plugins: [
+    tailwind(),
+    autoprefixer,
+  ],
+};
